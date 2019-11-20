1a9498c56226367512608da3143a463fa9283503
vagrant <vagrant@homestead>
2019-11-20 07:33:54 +0000

    feature/admin-login_ui delete

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -2,11 +2,7 @@
 
 namespace App\Http\Controllers\Backend;
 
-
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -18,32 +14,5 @@ class AdminController extends Controller
     public function index()
     {
         return view('admin.dashboards.index');
-    }
-    public function getlogin (){
-    	return view('Admin.login');
-    }
-    public function postlogin (Request $request) {
-        
-        if ($request->isMethod('post')) {
-                try {
-            $email = $request->post('email');
-            $password = $request->post('password');
-            $login= [
-            'email' => $email,
-            'password' => $password
-        ];
-            if (Auth::attempt($login)) {
-                return redirect()->route('users.index');
-            } else {
-                return redirect()->back();
-            }
-        }
-        
-        catch(\Exception $ex)
-                {
-                    // trả về mã lỗi ngoại lệ.
-                    return response()->json(['error' => $ex->getMessage(), 'field' => ''], 400);
-                }
-        }
     }
 }
