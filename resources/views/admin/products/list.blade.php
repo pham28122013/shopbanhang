e4397fe2be5ce99d2cc41d80276216bc6f44531e
vagrant <vagrant@homestead>
2019-12-05 09:18:38 +0000

    feature/admin_createProducts fix-mesage

--- resources/views/admin/products/list.blade.php
+++ resources/views/admin/products/list.blade.php
@@ -25,11 +25,10 @@
                             <button type="button" class="close" data-dismiss="alert">×</button>	
                                 <strong>{{ $message }}</strong>
                         </div>
-                    @endif
-                    @if ($message = Session::get('failed'))
-                        <div class="alert alert-success alert-block">
+                    @elseif ($message = Session::get('failed'))
+                        <div class="alert alert-danger alert-block">
                             <button type="button" class="close" data-dismiss="alert">×</button>	
-                                <strong>{{ $failed }}</strong>
+                                <strong>{{ $message }}</strong>
                         </div>
                     @endif
                 </div>
