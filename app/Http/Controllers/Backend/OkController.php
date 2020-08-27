747ee652f60655bbf1b455a1f0cee769fc3e2dd9
Di Pham <di.pham@boxyz.com>
2020-08-27 22:44:38 +0700

    commit 3

---
+++ app/Http/Controllers/Backend/OkController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+use DB;
+
+class OkController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
